daa44d0aa672e8e657e39ad1efb0b1efac034438
Nicolas <nicolasoutog@gmail.com>
2023-01-17 17:13:25 -0300

    Button now has a function

--- src/components/DaysForecast.js
+++ src/components/DaysForecast.js
@@ -1,38 +1,26 @@
-import React, { useEffect } from "react";
-import { getDaysWeather } from "../api/Api";
-import { selectDaysWeather, selectIsDaysWeatherLoaded, setDaysWeather, toggleIsLoaded } from "../slices/daysWeatherSlice";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { selectDaysWeather, selectIsDaysWeatherLoaded } from "../slices/daysWeatherSlice";
+import { useSelector } from "react-redux";
 import { FaTemperatureLow, FaTemperatureHigh } from 'react-icons/fa';
 
 const DaysForecast = () => {
-    const dispatch = useDispatch();
     const isLoaded = useSelector(selectIsDaysWeatherLoaded);
     const daysWeather = useSelector(selectDaysWeather);
-
-    useEffect(() => {
-        async function setData() {
-            dispatch(toggleIsLoaded);
-            const data = await getDaysWeather("São paulo");
-            dispatch(setDaysWeather({daysWeather: data}))
-        }
-
-        setData();
-    })
 
     return (
         <div className="card">
             <div className="card-title">7 Days Forecast</div>
             <div className="card-content flex-collumn">
-                {isLoaded ? daysWeather.forecast.forecastday.map(el => {
+                {isLoaded ? daysWeather.forecast.forecastday.map((el, idx) => {
                     return (
-                        <div>
+                        <div key={idx}>
                             <div className="day-forecast-card">
                                 <div className="info">
                                     {el.date}
                                 </div>
                                 <div className="card-figure">
                                     <div className="icon">
-                                        <img src={`https:${el.day.condition.icon}`} className='icon-img'/>
+                                        <img src={`https:${el.day.condition.icon}`} alt={`https:${el.day.condition.icon}`} className='icon-img'/>
                                         <div>{el.day.condition.text}</div>
                                     </div>
                                 </div>
